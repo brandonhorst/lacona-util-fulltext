40d200c662a76d927be349bef6ea014ee974597c
Brandon Horst <brandonhorst@gmail.com>
2015-01-16 09:18:28 -0500

    Modernized for Lacona

--- test/test.js
+++ test/test.js
@@ -1,31 +1,68 @@
 var expect = require('chai').expect;
 var fulltext = require('..');
 var lacona = require('lacona');
+var stream = require('stream');
+
+function toStream(strings) {
+  var newStream = new stream.Readable({objectMode: true});
+
+  strings.forEach(function (string) {
+    newStream.push(string);
+  });
+  newStream.push(null);
+
+  return newStream;
+}
+
+function toArray(done) {
+  var newStream = new stream.Writable({objectMode: true});
+  var list = [];
+  newStream.write = function(obj) {
+    list.push(obj);
+  };
+
+  newStream.end = function() {
+    done(list);
+  };
+
+  return newStream;
+}
 
 describe('fulltext', function () {
   var parser;
 
   beforeEach(function () {
-    parser = new lacona.Parser({sentences: ['test']});
+    parser = new lacona.Parser();
   });
 
   it('gives the full text for an OutputOption', function(done) {
-    var grammar = {
-      phrases: [{
-        name: 'test',
-        root: ['a', 'b', 'c']
-      }]
-    };
+    var test = lacona.createPhrase({
+      name: 'test/test',
+      describe: function () {
+        return lacona.sequence({children: [
+          lacona.literal({text: 'a '}),
+          lacona.literal({text: 'b '}),
+          lacona.literal({text: 'c'})
+        ]});
+      }
+    });
 
-    function handleData(data) {
-      var text = fulltext(data);
-      expect(text).to.equal('a b c');
+    function callback(data) {
+      expect(data).to.have.length(3);
+
+      var option = data[1].data;
+
+      expect(fulltext.all(option)).to.equal('a b c');
+      expect(fulltext.match(option)).to.equal('a ');
+      expect(fulltext.suggestion(option)).to.equal('b ');
+      expect(fulltext.completion(option)).to.equal('c');
       done();
     }
 
-    parser
-      .understand(grammar)
-      .on('data', handleData)
-      .parse('a ');
+    parser.sentences = [test()];
+
+    toStream(['a '])
+      .pipe(parser)
+      .pipe(toArray(callback));
   });
 });
