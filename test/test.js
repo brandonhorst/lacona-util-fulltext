00f5a67cc18f0afd91844c93c71d8399b5bda11f
Brandon Horst <brandonhorst@gmail.com>
2015-01-20 11:35:25 -0500

    Move to new suggestion style

--- test/test.js
+++ test/test.js
@@ -1,68 +1,35 @@
 var expect = require('chai').expect;
 var fulltext = require('..');
-var lacona = require('lacona');
-var stream = require('stream');
-
-function toStream(strings) {
-  var newStream = new stream.Readable({objectMode: true});
-
-  strings.forEach(function (string) {
-    newStream.push(string);
-  });
-  newStream.push(null);
-
-  return newStream;
-}
-
-function toArray(done) {
-  var newStream = new stream.Writable({objectMode: true});
-  var list = [];
-  newStream.write = function(obj) {
-    list.push(obj);
-  };
-
-  newStream.end = function() {
-    done(list);
-  };
-
-  return newStream;
-}
 
 describe('fulltext', function () {
-  var parser;
 
-  beforeEach(function () {
-    parser = new lacona.Parser();
-  });
+  it('gives the full text for an OutputOption', function() {
+    var option = {
+      match: [{
+        string: 'aaa',
+        category: 'cata'
+      }], suggestion: [{
+        string: 'b',
+        category: 'catb',
+        input: true
+      }, {
+        string: 'c',
+        category: 'catb',
+        input: false
+      }, {
+        string: 'd',
+        category: 'catb',
+        input: true
+      }], completion: [{
+        string: 'eee',
+        category: 'cate'
+      }]
+    };
 
-  it('gives the full text for an OutputOption', function(done) {
-    var test = lacona.createPhrase({
-      name: 'test/test',
-      describe: function () {
-        return lacona.sequence({children: [
-          lacona.literal({text: 'a '}),
-          lacona.literal({text: 'b '}),
-          lacona.literal({text: 'c'})
-        ]});
-      }
-    });
 
-    function callback(data) {
-      expect(data).to.have.length(3);
-
-      var option = data[1].data;
-
-      expect(fulltext.all(option)).to.equal('a b c');
-      expect(fulltext.match(option)).to.equal('a ');
-      expect(fulltext.suggestion(option)).to.equal('b ');
-      expect(fulltext.completion(option)).to.equal('c');
-      done();
-    }
-
-    parser.sentences = [test()];
-
-    toStream(['a '])
-      .pipe(parser)
-      .pipe(toArray(callback));
+    expect(fulltext.all(option)).to.equal('aaabcdeee');
+    expect(fulltext.match(option)).to.equal('aaa');
+    expect(fulltext.suggestion(option)).to.equal('bcd');
+    expect(fulltext.completion(option)).to.equal('eee');
   });
 });
